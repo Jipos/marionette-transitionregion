6b04feaad9947e06f7efa0ced04cbe930cc2aa74
Jipos <nonoma@hotmail.com>
2015-02-26 19:14:51 +0100

    added a new compiled version

--- transitionRegion.js
+++ transitionRegion.js
@@ -1,20 +1,23 @@
 var extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
   hasProp = {}.hasOwnProperty;
 
-(function(root, factory) {
+(function(factory) {
   var Marionette;
   if (typeof define === 'function' && define.amd) {
     return define(['backbone.marionette'], function(marionette) {
-      return factory(root, marionette);
+      return factory(marionette);
     });
   } else if (typeof exports === 'object') {
     Marionette = require('backbone.marionette');
-    return module.exports = factory(root, Marionette);
+    return module.exports = factory(Marionette);
   } else {
-    return factory(root, Backbone.Marionette);
+    return factory(Backbone.Marionette);
   }
-})(this, function(Marionette) {
-  var FadeTransitionRegion, getFadeDuration;
+})(function(Marionette) {
+  var FadeTransitionRegion, delay, getFadeDuration;
+  delay = function(duration, callback) {
+    return setTimeout(callback, duration);
+  };
   getFadeDuration = function(region) {
     return this.region.transitionDuration || 100;
   };
@@ -29,7 +32,7 @@ var extend = function(child, parent) { for (var key in parent) { if (hasProp.cal
       if (typeof this.skipTransition === "function" ? this.skipTransition(view) : void 0) {
         return FadeTransitionRegion.__super__.attachHtml.call(this, view);
       } else {
-        return setTimeout((function(_this) {
+        return delay(duration, (function(_this) {
           return function() {
             var duration;
             duration = _this.transitionDuration || 100;
@@ -38,7 +41,7 @@ var extend = function(child, parent) { for (var key in parent) { if (hasProp.cal
             _this.el.appendChild(view.el);
             return view.$el.fadeIn(fadeDuration);
           };
-        })(this), duration);
+        })(this));
       }
     };
 
